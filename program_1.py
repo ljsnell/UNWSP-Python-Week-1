897c15cc30ecb21c3e3b014467b86f691bf37b7c
ljsnell <ljsnell7@gmail.com>
2024-08-24 10:30:29 -0500

    Updated to make the initial programs runnable

--- program_1.py
+++ program_1.py
@@ -2,3 +2,5 @@ def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
     print('Hello ...')
+
+hello_world()
--- program_2.py
+++ program_2.py
@@ -1,3 +1,6 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
+    print('Hi')
     
+
+proverbs_22_29()
