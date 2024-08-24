00ebb57b116180f670ff18072a0077806067c4a4
ljsnell <ljsnell7@gmail.com>
2024-08-24 11:16:11 -0500

    Test

--- program_1.py
+++ program_1.py
@@ -1,6 +1,6 @@
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+    print('Hello World')
 
 hello_world()
--- program_2.py
+++ program_2.py
@@ -1,6 +1,6 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('Hi')
+    print('Proverbs 22:29')
     
 
 proverbs_22_29()
